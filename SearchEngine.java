f3a56b0c761037685bcd8ed3325418a1dd2f04c0
Rashid Abder Rahim <rabder24@gmail.com>
2023-04-13 17:30:44 -0700

    Made changes to NumberServer and created a Search Engine!

---
+++ SearchEngine.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> stringlst = new ArrayList<>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Search engine");
+        }
+        else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("insert")) {
+                    stringlst.add(parameters[1]);
+                    return String.format("String " + parameters[1] + " added to list!");
+                }
+            }
+            else if (url.getPath().contains("/lookup")){
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("search")) {
+                    if (stringlst.contains(parameters[1])){
+                        return String.format("String " + parameters[1] + " is in the list!");
+                    }
+                    else{
+                        return String.format("Not in the list. Feel free to add it");
+                    }
+                }
+            }
+            return "404 Not Found!";
+        }
+}
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
